41064258a4509722fad513481efb26fe74b9602a
Eleazar Meza <meza.eleazar@gmail.com>
2021-06-23 14:44:58 -0400

    Commit inicial

---
+++ my_rotate.rb
@@ -0,0 +1,16 @@
+def my_rotate(a, k)
+  n = a.length
+  rotate = k % n
+
+  return a if rotate == 0
+
+  b = []
+  a.each_with_index do |ai, i|
+    rotated_i = i + rotate
+    rotated_i -= n if rotated_i > n - 1
+
+    b[rotated_i] = ai
+  end
+
+  b
+end
